86fa6f460e91d34723c90ff833025ec312c73dda
JaredSelling <jstephen2014@fau.edu>
2016-10-26 13:14:34 -0400

    Add files via upload
    start of the admin view.  Uses jquery to load different divs depending on the buttons clicked from the sidebar.

---
+++ adminHomepage/javascript/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    
+   $('.navbutton').on('click', function(e) {
+      e.preventDefault();
+       $('.content').hide();
+       $('#' + this.id + 'Div').show();
+   });
+    
+    $('')
+    
+    
+    //ALLOW EDITING OF TUTOR PROFILE
+    $('#editBtn').on('click', function() {
+        $(this).closest('body').find('.info').each(function() {
+            
+            if(!$(this).parent().hasClass('edit')) {    
+               $(this).parent().addClass('edit'); 
+            
+                var oldText = $(this).closest('body').find($(this)).text();
+                
+                $(this).find('span').remove();
+                
+                if(!$(this).is('#name')) {
+                     
+
+                     $(this).append('<form><span><textarea rows="3" cols="50" contenteditable="true">' + oldText + '</textarea></span></form>');
+
+
+                    
+                }
+                else {
+                    $(this).append('<form><span><textarea rows="1" cols="20" contenteditable="true">' + oldText + '</textarea></span></form>')
+                }
+                
+               $(this).closest('body').find('#editBtn').text('Save');
+
+            }
+            
+            else {
+                var newText = $(this).parent().find('textarea').val();
+                $(this).find('span').text(newText);
+                $(this).closest('body').find('#editBtn').text('Edit');
+                newText = '';
+                $(this).parent().removeClass('edit');
+            }
+            
+            
+            
+        });
+        
+    });
+    
+});
