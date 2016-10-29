b49274fb1347b5e8ac9ad45ff3118d4667701a8c
JaredSelling <jstephen2014@fau.edu>
2016-10-29 09:19:32 -0400

    Admin view with homepage, message page, and edit tutor profile page

--- adminHomepage/javascript/script.js
+++ adminHomepage/javascript/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
        $('#' + this.id + 'Div').show();
     }
     
-
+    
     
    $('.navbutton').on('click', displayDiv);
 
@@ -53,6 +53,26 @@ $(document).ready(function() {
             
         });
         
+    });
+    
+    
+    //DISPLAY MESSAGES 
+    $('.star').on('click', function () {
+      $(this).toggleClass('star-checked');
+    });
+
+    $('.ckbox label').on('click', function () {
+      $(this).parents('tr').toggleClass('selected');
+    });
+
+    $('.btn-filter').on('click', function () {
+      var $target = $(this).data('target');
+      if ($target != 'all') {
+        $('.table tr').css('display', 'none');
+        $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+      } else {
+        $('.table tr').css('display', 'none').fadeIn('slow');
+      }
     });
     
 });
