69db806f735f99c30e64f98859e309d37325f8b4
John Hernandez <john.m.hernandez@gmail.com>
2016-11-27 18:12:25 -0500

    Add files via upload

--- apiTest/controllers/post.php
+++ apiTest/controllers/post.php
@@ -4,6 +4,8 @@ require_once("classes/dataClassinfo.php");
 require_once("classes/dataLocation.php");
 require_once("classes/dataMain.php");
 require_once("classes/dataStudent.php");
+require_once("classes/dataTutor.php");
+require_once("classes/dataClasssession.php");
 
 
 
@@ -14,31 +16,42 @@ class post
 	public function updateDirectorAction($data,$params)	{ $dataClass = new dataDirector(); 	return $dataClass->updateAction($data,$params); }
 	public function deleteDirectorAction($data,$params) 	{ $dataClass = new dataDirector(); 	return $dataClass->deleteAction($data,$params); }
 	public function getDirectorsAction($data,$params)	{ $dataClass = new dataDirector(); 	return $dataClass->getAction($data,$params);  }	
-	public function getDirectornamesAction($data,$params){ $dataClass = new dataDirector(); 	return $dataClass->getnamesAction($data,$params);  }	
+	public function getDirectornamesAction($data,$params) { $dataClass = new dataDirector(); 	return $dataClass->getnamesAction($data,$params);  }	
 	
 	public function addClassinfoAction($data,$params)	{ $dataClass = new dataClassinfo(); return $dataClass->addAction($data,$params); }
 	public function updateClassinfoAction($data,$params)	{ $dataClass = new dataClassinfo(); return $dataClass->updateAction($data,$params); }
 	public function deleteClassinfoAction($data,$params) 	{ $dataClass = new dataClassinfo(); return $dataClass->deleteAction($data,$params); }
 	public function getClassinfosAction($data,$params)	{ $dataClass = new dataClassinfo(); return $dataClass->getAction($data,$params); }	
+	public function getClassinfonamesAction($data,$params){ $dataClass = new dataClassinfo(); return $dataClass->getnamesAction($data,$params);  }	
 
 	
 	public function addLocationAction($data,$params)	{ $dataClass = new dataLocation(); 	return $dataClass->addAction($data,$params); }
 	public function updateLocationAction($data,$params)	{ $dataClass = new dataLocation(); 	return $dataClass->updateAction($data,$params); }
 	public function deleteLocationAction($data,$params) 	{ $dataClass = new dataLocation(); 	return $dataClass->deleteAction($data,$params); }
 	public function getLocationsAction($data,$params)	{ $dataClass = new dataLocation(); 	return $dataClass->getAction($data,$params); }
+	public function getLocationnamesAction($data,$params) { $dataClass = new dataLocation(); return $dataClass->getnamesAction($data,$params);  }	
 
 	public function addStudentAction($data,$params)		{ $dataClass = new dataStudent(); 	return $dataClass->addAction($data,$params); }
 	public function updateStudentAction($data,$params)	{ $dataClass = new dataStudent(); 	return $dataClass->updateAction($data,$params); }
 	public function deleteStudentAction($data,$params) 	{ $dataClass = new dataStudent(); 	return $dataClass->deleteAction($data,$params); }
 	public function getStudentsAction($data,$params)	{ $dataClass = new dataStudent(); 	return $dataClass->getAction($data,$params); }	
+	public function getStudentnamesAction($data,$params)	{ $dataClass = new dataStudent(); 	return $dataClass->getnamesAction($data,$params);  }	
 	
+	public function addTutorAction($data,$params)		{ $dataClass = new dataTutor(); 	return $dataClass->addAction($data,$params); }
+	public function updateTutorAction($data,$params)	{ $dataClass = new dataTutor(); 	return $dataClass->updateAction($data,$params); }
+	public function deleteTutorAction($data,$params) 	{ $dataClass = new dataTutor(); 	return $dataClass->deleteAction($data,$params); }
+	public function getTutorsAction($data,$params)		{ $dataClass = new dataTutor(); 	return $dataClass->getAction($data,$params); }	
+	public function getTutornamesAction($data,$params)	{ $dataClass = new dataTutor(); 	return $dataClass->getnamesAction($data,$params);  }	
 	
-	
-	
+	public function addClasssessionAction($data,$params)		{ $dataClass = new dataClasssession(); 	return $dataClass->addAction($data,$params); }
+	public function updateClasssessionAction($data,$params)	{ $dataClass = new dataClasssession(); 	return $dataClass->updateAction($data,$params); }
+	public function deleteClasssessionAction($data,$params) 	{ $dataClass = new dataClasssession(); 	return $dataClass->deleteAction($data,$params); }
+	public function getClasssessionAction($data,$params)		{ $dataClass = new dataClasssession(); 	return $dataClass->getAction($data,$params); }	
+
 
 	public function getlistAction($data,$params)		{ $dataClass = new dataMain(); 		return $dataClass->getlistAction($data,$params); }
-	
-	
+	public function getdashboardlistAction($data,$params)	{ $dataClass = new dataMain(); 		return $dataClass->getdashboardlistAction($data,$params); }
+
 	
 
 }
