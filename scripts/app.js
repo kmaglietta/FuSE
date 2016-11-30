f9cdbb66991862091d615b25226cd84d3cd3751a
Moss Pakhapoca <thorsleepless@gmail.com>
2016-11-30 12:09:04 -0500

    Make localstage delete specific variable

--- scripts/app.js
+++ scripts/app.js
@@ -140,14 +140,6 @@ angular
       return $location.path();
     });
 
-  })
-  .constant('AUTH_EVENTS', {
-    loginSuccess: 'auth-login-success',
-    loginFailed: 'auth-login-failed',
-    logoutSuccess: 'auth-logout-success',
-    sessionTimeout: 'auth-session-timeout',
-    notAuthenticated: 'auth-not-authenticated',
-    notAuthorized: 'auth-not-authorized'
   });
 
 
--- scripts/controllers/dashboard.js
+++ scripts/controllers/dashboard.js
@@ -5,14 +5,14 @@ angular.module('tossApp')
     var ctrl = this;
     ctrl.userData = [];
     ctrl.userData = $localStorage.userData;
-    
+
     ctrl.logout = function(){
       // Log user out
       $log.log('logging out...');
       userService.logout().then(function (data) {
-        //$rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
         // Clear localStorage data
-        $localStorage.$reset();
+        //$localStorage.$reset();
+        delete $localStorage.userData;
         $injector.get('$state').go('login');
       }, function(data) {
         $log.error('An error has occurred');
